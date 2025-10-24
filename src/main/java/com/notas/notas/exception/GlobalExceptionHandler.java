c0a8ca82e4b9561fecb7ce6102e5e37c78f9bbff
BryanCoSp <134606853+BryanCoSp@users.noreply.github.com>
2025-10-24 18:38:41 +0200

    Add example for global exception handler

--- src/main/java/com/notas/notas/exception/GlobalExceptionHandler.java
+++ src/main/java/com/notas/notas/exception/GlobalExceptionHandler.java
@@ -1,5 +1,17 @@
 package com.notas.notas.exception;
 
-public class GlobalExceptionHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NoteNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoteNotFound(NoteNotFoundException ex){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        ErrorResponse error = new ErrorResponse(status.value(), ex.getMessage());
+        return new ResponseEntity<>(error, status);
+    }
 }
