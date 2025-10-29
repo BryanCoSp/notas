31125371ca5291fb7653e8b14b44f182612f7c3d
BryanCoSp <134606853+BryanCoSp@users.noreply.github.com>
2025-10-29 19:17:21 +0100

    Add basic security to app

--- src/main/java/com/notas/notas/config/WebConfig.java
+++ src/main/java/com/notas/notas/config/WebConfig.java
@@ -1,13 +1,33 @@
-//package com.notas.notas.config;
+package com.notas.notas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
+}
+
+//@Test
+//void debeCrearUnaNuevaNotePorApi() throws Exception {
+//    //Arrange
+//    String newNoteJson = "{\"title\": \"Note title\",\"description\": \"Noteeeeeee2 test\", \"completed\":false}";
 //
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.filter.HiddenHttpMethodFilter;
+//    //Act and Assert
 //
-//@Configuration
-//public class WebConfig {
-//    @Bean
-//    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
-//        return new HiddenHttpMethodFilter();
-//    }
+//    mockMvc.perform(post("/api/notas")
+//                    .with(user("testUser").roles("USER"))
+//                    .with(csrf())
+//                    .contentType(MediaType.APPLICATION_JSON)
+//                    .content(newNoteJson))
+//            .andExpect(status().isOk())
+//            .andExpect(jsonPath("$.description").value("Noteeeeeee2 test"))
+//            .andExpect(jsonPath("$.id").exists());
+//
+//
 //}
+
--- src/main/java/com/notas/notas/controller/AuthController.java
+++ src/main/java/com/notas/notas/controller/AuthController.java
@@ -1,54 +1,55 @@
-//package com.notas.notas.controller;
-//
-//import com.notas.notas.model.AppUser;
-//import com.notas.notas.repository.AppUserRepository;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.validation.annotation.Validated;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import jakarta.validation.constraints.NotBlank;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.PostMapping;
-//
-//@Controller
-//@Validated
-//public class AuthController {
-//    private final AppUserRepository appUserRepository;
-//    private final PasswordEncoder passwordEncoder;
-//
-//    public AuthController(AppUserRepository appUserRepository, PasswordEncoder passwordEncoder ) {
-//        this.appUserRepository = appUserRepository;
-//        this.passwordEncoder = passwordEncoder;
-//    }
-//
-//    @GetMapping("/login")
-//    public String login() {
-//        return "login";
-//    }
-//
-//    @GetMapping("/register")
-//    public String register(Model model) {
-//        model.addAttribute("userDTO", new RegisterDTO("",""));
-//        return "register";
-//    }
-//
-//    @PostMapping("/register")
-//    public String register(@ModelAttribute("userDTO") @Validated RegisterDTO registerDTO,Model model){
-////        if(appUserRepository.existsByUserName(registerDTO.userName)){
-////            model.addAttribute("error","El usuario ya existe");
-////            return "register";
-////        }
-////        AppUser appUser = new AppUser();
-////        appUser.setUserName(registerDTO.userName);
-////        appUser.setPassword(passwordEncoder.encode(registerDTO.password));
-////        appUser.setRol("ROLE_USER");
-////        appUserRepository.save(appUser);
-////
-////        return "redirect:/login";
-//        return "register";
-//    }
-//
-//    public record RegisterDTO(@NotBlank String userName, @NotBlank String password){}
-//
-//}
+package com.notas.notas.controller;
+
+import com.notas.notas.model.AppUser;
+import com.notas.notas.repository.AppUserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Validated
+public class AuthController {
+    private final AppUserRepository appUserRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(AppUserRepository appUserRepository, PasswordEncoder passwordEncoder ) {
+        this.appUserRepository = appUserRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "redirect:/";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("userDTO", new RegisterDTO("",""));
+        System.out.println("register view");
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute("userDTO") @Validated RegisterDTO registerDTO,Model model){
+        System.out.println("register REGISTRO");
+        if(appUserRepository.existsByUserName(registerDTO.userName)){
+            model.addAttribute("error","El usuario ya existe");
+            return "register";
+        }
+        AppUser appUser = new AppUser();
+        appUser.setUserName(registerDTO.userName);
+        appUser.setPassword(passwordEncoder.encode(registerDTO.password));
+        appUser.setRol("ROLE_USER");
+        appUserRepository.save(appUser);
+
+        return "redirect:/login";
+    }
+
+    public record RegisterDTO(@NotBlank String userName, @NotBlank String password){}
+
+}
